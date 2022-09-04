d16572da1a4833bb66317f74bac508b68047af49
arale275 <arale275@gmail.com>
2022-09-04 03:46:26 +0300

    Changes to be committed: 	new file:   .vscode/settings.json 	modified:   index.html 	modified:   scripts/index.js 	modified:   styles/index.css

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,44 @@
-document.addEventListener("click", e => {
-  const isDropdownButton = e.target.matches("[data-dropdown-button]")
-  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+/* the header style */
 
-  let currentDropdown
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
+
+  let currentDropdown;
   if (isDropdownButton) {
-    currentDropdown = e.target.closest("[data-dropdown]")
-    currentDropdown.classList.toggle("active")
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
   }
 
-  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-    if (dropdown === currentDropdown) return
-    dropdown.classList.remove("active")
-  })
-})
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
+});
+
+/* the header change style */
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".change_bg");
+
+
+const sectionOneOptions = {
+  rootMargin: "-50px 0px 0px 0px",
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
