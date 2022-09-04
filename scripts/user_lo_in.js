c8e2141a3983d32da53e4560c3f5a70ce69267fc
arale275 <arale275@gmail.com>
2022-09-04 16:17:37 +0300

    Changes to be committed: 	modified:   index.html 	modified:   scripts/index.js 	modified:   scripts/user_lo_in.js

--- scripts/user_lo_in.js
+++ scripts/user_lo_in.js
@@ -1,25 +1,24 @@
 /* sign up */
 
 try {
-    const { user } = await Auth.signUp({ username, password });
-    console.log(user);
+  const { user } = await Auth.signUp({ username, password });
+  console.log(user);
 } catch (error) {
-    console.log('error signing up:', error);
+  console.log("error signing up:", error);
 }
 
 /* sign in */
 
 try {
-    const user = await Auth.signIn(username, password);
+  const user = await Auth.signIn(username, password);
 } catch (error) {
-    console.log('error signing in', error);
+  console.log("error signing in", error);
 }
 
 /* sign out */
 
 try {
-    await Auth.signOut();
+  await Auth.signOut();
 } catch (error) {
-    console.log('error signing out: ', error);
+  console.log("error signing out: ", error);
 }
-
