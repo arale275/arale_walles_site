c209e24626a08e3691fb34b42572b0bf08a04814
arale275 <arale275@gmail.com>
2022-09-04 22:31:55 +0300

    Changes to be committed: 	modified:   index.html 	modified:   scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -42,22 +42,3 @@ const sectionOneObserver = new IntersectionObserver(function (
 sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
-
-
-
-/* code by google - log in API */
-
-function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
-  var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
-
-  // The ID token you need to pass to your backend:
-  var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
-}
