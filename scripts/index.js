5bff7dc92496b073130186467c78696217d7904b
arale275 <arale275@gmail.com>
2022-09-18 13:00:49 +0300

    modified:   scripts/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -41,59 +41,9 @@ sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
 
-
-/* animate */
-
-let words = ['אתר ייחודי', 'מותאם אישית'],
-    part,
-    i = 0,
-    offset = 0,
-    len = words.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 15,
-    speed = 70;
-let wordflick = function () {
-  setInterval(function () {
-    if (forwards) {
-      if (offset >= words[i].length) {
-        ++skip_count;
-        if (skip_count == skip_delay) {
-          forwards = false;
-          skip_count = 0;
-        }
-      }
-    }
-    else {
-      if (offset == 0) {
-        forwards = true;
-        i++;
-        offset = 0;
-        if (i >= len) {
-          i = 0;
-        }
-      }
-    }
-    part = words[i].substr(0, offset);
-    if (skip_count == 0) {
-      if (forwards) {
-        offset++;
-      }
-      else {
-        offset--;
-      }
-    }
-    jQuery('.word').text(part);
-  },speed);
-};
-
-jQuery(document).ready(function () {
-  wordflick();
-});
-
 /* animation */
 
-var aText = new Array(
+let aText = new Array(
   "ברוכים הבאים!!!",
   "\n",
   " צריכים אתר ייחודי ומותאם אישית ?",
